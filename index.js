44e7867b3c37ba7172d317caecfcb88654f37c73
KyubumShin <kbum0617@naver.com>
2020-08-16 21:41:52 +0900

    1st Practice

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Im Working');
+const title = document.getElementById("title");
+title.innerHTML = "Hi! I'm first from JS";
+title.style.backgroundColor = "red";
+document.title = "It's Mine"
+console.dir(title);
